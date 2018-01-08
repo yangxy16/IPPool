0c811a31d3dcdf0088af35b21901177d4da98d6c
yangxy16 <yangxy16@lenovo.com>
2018-01-08 09:55:09 +0800

    update

--- ProxyTest.py
+++ ProxyTest.py
@@ -8,17 +8,20 @@
 import requests as rq
 
 if __name__ == '__main__':
-    ip = '122.205.19.30'
-    port = '8088'
+    ip = '120.77.201.46'
+    port = '8080'
     
-    url = 'http://2017.ip138.com/ic.asp'
-    proxies = { 'http': 'http://' + ip + ':' + port }
-    try:
-        r = rq.get( url, proxies = proxies, timeout = 10 )
-        if r.status_code == 200:
-            html = r.content.decode( 'gb2312' )
-            print( html )
-        else:
-            print( '代理无效' )
-    except:
-        print( '代理无效' )
+    def getAvailableIP( ip, port ):
+        url = 'http://2017.ip138.com/ic.asp'
+        proxies = { 'http': 'http://' + ip + ':' +  port }
+        try:
+            r = rq.get( url, proxies = proxies, timeout = 10 )
+            if r.status_code == 200:
+                html = r.content.decode( 'gb2312' )
+                print( html )
+            else:
+                print( r.status_code )
+        except Exception as e:
+            print( e )
+            
+    getAvailableIP( ip, port )
